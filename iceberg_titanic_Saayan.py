2bc4fa8c41cb98edc5df33d2f4ace939a961bdcc
Sabie9 <42497416+Sabie9@users.noreply.github.com>
2018-09-01 13:28:25 +0530

    iceberg_titanic_Saayan_final

--- iceberg_titanic_Saayan.py
+++ iceberg_titanic_Saayan.py
@@ -7,6 +7,9 @@ data1=pd.read_csv('test_X.csv')
 data2=pd.read_csv('test_y.csv')
 data.Sex=data.Sex.replace('male',1).replace('female',0)
 data1.Sex=data1.Sex.replace('male',1).replace('female',0)
+data.Embarked=data.Embarked.replace('S',1).replace('C',0).replace('Q',2)
+data1.Embarked=data1.Embarked.replace('S',1).replace('C',0).replace('Q',2)
+
 
 
 data['Cabin']=data['Cabin'].fillna(0)
@@ -17,17 +20,10 @@ data1['Cabin']=data1['Cabin'].fillna(0)
 count1=data1['Cabin'].value_counts().to_dict()
 data1=data1.replace({"Cabin":count1})
 
-train1=data.drop(['PassengerId','Survived','Name','Ticket','Embarked'],axis=1)
-test1=data1.drop(['PassengerId','Name','Ticket','Embarked'],axis=1)
+train1=data.drop(['PassengerId','Survived','Name','Ticket','Fare'],axis=1)
+test1=data1.drop(['PassengerId','Name','Ticket','Fare'],axis=1)
 test2=data2.drop(['PassengerId'],axis=1)
 
-
-
-
-
-#train1=data.drop(['PassengerId','Survived','Name','Ticket','Cabin','Embarked'],axis=1)
-#test1=data1.drop(['PassengerId','Name','Ticket','Cabin','Embarked'],axis=1)
-#test2=data2.drop(['PassengerId'],axis=1)
 
 
 from sklearn.linear_model import LogisticRegression
@@ -45,4 +41,3 @@ print("Train data score")
 print(reg.score(train1,data['Survived']))
 print("Test data score")
 print(reg.score(test1,test2))
-
